4df3f189e270ec7091217d6c4230ce729172c013
Andrey Matvienko <andrey_matv@mail.ru>
2017-05-14 01:16:08 +0300

    added section package_versions

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,33 @@ $(document).ready(function(){
 	navigationText: false,
 	items: 1,
 	});
-})
+
+
+    $('body').on('mouseenter', '.first', function(e){
+        $(this).css('background-color', '#49cbcd');
+        $('.first_p').css('background-color', '#49cbcd');
+    }).on('mouseout', '.first', function(e){
+        $(this).css('background-color', '#788492');
+        $('.first_p').css('background-color', '#485460');
+    });
+	
+	$('body').on('mouseenter', '.second', function(e){
+        $(this).css('background-color', '#49cbcd');
+        $('.second_p').css('background-color', '#49cbcd');
+    }).on('mouseout', '.second', function(e){
+        $(this).css('background-color', '#788492');
+        $('.second_p').css('background-color', '#485460');
+    });
+
+    $('body').on('mouseenter', '.third', function(e){
+        $(this).css('background-color', '#49cbcd');
+        $('.third_p').css('background-color', '#49cbcd');
+    }).on('mouseout', '.third', function(e){
+        $(this).css('background-color', '#788492');
+        $('.third_p').css('background-color', '#485460');
+    });
+
+
+});
+
+	
